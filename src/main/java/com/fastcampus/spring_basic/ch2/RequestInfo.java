48b6da743018c5fa3f5babba8db2143c0688e051
backwizard0 <gkaeogus7@gmail.com>
2023-03-26 22:44:28 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/spring_basic/ch2/RequestInfo.java
@@ -0,0 +1,15 @@
+package com.fastcampus.spring_basic.ch2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class RequestInfo {
+    @RequestMapping("/requestInfo")
+    public void main(HttpServletRequest request) {
+        System.out.println();
+    }
+
+}
